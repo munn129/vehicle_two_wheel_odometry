3d608df836a774c2d3b60fd5c2532a86d6be5680
moon_home <ans92175@gmail.com>
2024-07-11 23:22:59 +0900

    인터럽트 핸들러 분리

---
+++ interrupt_handler.py
@@ -0,0 +1,10 @@
+import signal
+import sys
+
+class Activate_Signal_Interrupt_Handler:
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.signal_handler)
+
+    def signal_handler(self, sig, frame):
+        print('\nYou pressed Ctrl+C! Never use Ctrl+Z!')
+        sys.exit(0)
